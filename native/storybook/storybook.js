8ecb59121ee171ca5a1fc514e562a644ed9f0eb4
Shaun Donnelly <shaundonnelly@gmail.com>
2017-09-18 16:28:50 +0100

    [BPK-869] Removed something that shouldn't have been committed

--- native/storybook/storybook.js
+++ native/storybook/storybook.js
@@ -11,6 +11,6 @@ configure(() => {
 
 // This assumes that storybook is running on the same host as your RN packager,
 // to set manually use, e.g. host: 'localhost' option
-const StorybookUI = getStorybookUI({ port: 7007, onDeviceUI: false });
+const StorybookUI = getStorybookUI({ port: 7007 });
 AppRegistry.registerComponent('native', () => StorybookUI);
 export default StorybookUI;
